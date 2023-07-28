62f6b29f44b6deb87c064edbf4511b81eec3a469
pawelofficial <zdunekpawel@protonmail.com>
2023-07-28 20:48:17 +0200

    update

--- __pz/snow2tweet/loop.sh
+++ __pz/snow2tweet/loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    /home/marekneural/tests/venv/bin/python3 /home/marekneural/tests/v1/book_excerpts/oa.py >> /home/marekneural/tests/v1/book_excerpts/log.log
+    /home/marekneural/tests/venv/bin/python3 /home/marekneural/tests/snow2/__pz/oa.py >> /home/marekneural/tests/snow2/__pz/log.log
     sleep 7200
 done
