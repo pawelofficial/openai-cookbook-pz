85a2d08bf69290da75c6b41658a01dbd00c65c49
pawelofficial <zdunekpawel@protonmail.com>
2023-07-28 20:45:35 +0200

    add loop sh

---
+++ __pz/snow2tweet/loop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+    /home/marekneural/tests/venv/bin/python3 /home/marekneural/tests/v1/book_excerpts/oa.py >> /home/marekneural/tests/v1/book_excerpts/log.log
+    sleep 7200
+done
